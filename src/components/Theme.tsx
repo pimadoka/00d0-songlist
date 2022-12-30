d66e82ce35f22ed41c7b25c05945ebf6939aaac4
pimadoka <pi_madoka@outlook.com>
2022-12-31 01:23:31 +0800

    feat: 监听系统theme,保存theme设置

--- src/components/Theme.tsx
+++ src/components/Theme.tsx
@@ -1,5 +1,5 @@
-import React, { useContext, useState } from 'react';
-import { Theme } from '@/common/styles';
+import React, { useContext, useEffect, useState } from 'react';
+import { CommonStyles, Theme } from '@/common/styles';
 
 interface ThemeContextProps {
   theme: Theme;
@@ -11,12 +11,50 @@ const ThemeContext = React.createContext<ThemeContextProps>({
   setTheme: () => {},
 });
 
+const ThemeLocalStorageKey = 'theme';
+
+const getSystemThemeMedia = () => window.matchMedia("(prefers-color-scheme: light)");
+
+const getSystemTheme = (): Theme => {
+  const themeMedia = getSystemThemeMedia();
+  return themeMedia.matches ? 'light' : 'dark';
+}
+  
+const getDefaultTheme = (): Theme => {
+  const savedTheme = localStorage.getItem(ThemeLocalStorageKey);
+  return savedTheme === 'light' || savedTheme === 'dark' ? savedTheme : getSystemTheme();
+}
+
+const changeRootThemeStyle = (theme: Theme) => {
+  const rootElement = document.documentElement;
+  rootElement.style.background = CommonStyles.Colors[theme].bg.secondary;
+  rootElement.style.color = CommonStyles.Colors[theme].fg.primary;
+}
+
 export const ThemeProvider: React.FC<{
   children: React.ReactNode;
 }> = ({
   children,
 }) => {
-  const [theme, setTheme] = useState<Theme>('light');
+  const [theme, setTheme] = useState<Theme>(getDefaultTheme());
+
+  useEffect(() => {
+    localStorage.setItem(ThemeLocalStorageKey, theme);
+    changeRootThemeStyle(theme);
+  }, [theme]);
+  
+  const onSystemThemeChange = (e: MediaQueryListEvent) => {
+    const systemTheme = e.matches ? 'light' : 'dark';
+    setTheme(systemTheme);
+  }
+
+  useEffect(() => {
+    const themeMedia = getSystemThemeMedia();
+    themeMedia.addEventListener('change', onSystemThemeChange);
+    return () => {
+      themeMedia.removeEventListener('change', onSystemThemeChange);
+    };
+  }, []);
 
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
